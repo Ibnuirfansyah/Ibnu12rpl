7c746e0b4c4895f9769531d2b0f6a5f49f7767e9
Ibnuirfansyah <you@example.com>
2020-07-28 20:35:18 +0700

    first commit

---
+++ app/src/main/java/com/example/ibnu12rpl2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ibnu12rpl2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
